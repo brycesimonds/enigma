8b76fe7b9f2ee8bd66513786488a875311fa87ce
Bryce Simonds <bsimonds14@gmail.com>
2022-06-10 10:26:42 -0600

    Test: string of 5 random numbers

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -13,4 +13,9 @@ RSpec.describe Key do
     expect(@key.array_random_five_nums).to be_a(Array)
     expect(@key.array_random_five_nums.count).to eq(5)
   end
+
+  it 'can create an String of 5 random numbers' do
+    expect(@key.random_num_string).to be_a(String)
+    expect(@key.random_num_string.length).to eq(5)
+  end
 end
