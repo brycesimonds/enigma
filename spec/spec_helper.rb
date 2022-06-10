5d807c992a9c6c0c44fee9b70515fa1061a2dd60
Bryce Simonds <bsimonds14@gmail.com>
2022-06-10 12:02:28 -0600

    Added require relative offset spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'simplecov'
 SimpleCov.start
 
 require_relative './key_spec'
+require_relative './offset_spec'
 #remember to require_relative files as you go
 #use rspec to run
 #open coverage/index.html opens website
