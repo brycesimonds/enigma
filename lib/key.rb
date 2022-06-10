e192d89e0ba77168605d0c21f2becbe268d66d71
Bryce Simonds <bsimonds14@gmail.com>
2022-06-10 10:27:12 -0600

    Feat: random num string complete

--- lib/key.rb
+++ lib/key.rb
@@ -7,4 +7,8 @@ class Key
   def array_random_five_nums #returns something like [1, 0, 6, 5, 2]
     (0..9).to_a.sample(5)
   end
+
+  def random_num_string #returns random 5 digit STRING, "46543"
+    array_random_five_nums.join(" ").delete(' ')
+  end
 end
