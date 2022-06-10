5933b396c3f77b6eda1f7bcbb9a15f72b7d645dc
Bryce Simonds <bsimonds14@gmail.com>
2022-06-10 10:25:34 -0600

    Test: create 5 random numbers array

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -4,7 +4,13 @@ RSpec.describe Key do
   before :each do
     @key = Key.new
   end
+
   it 'exists' do
     expect(@key).to be_a(Key)
   end
-end 
+
+  it 'can create an array of 5 random numbers' do
+    expect(@key.array_random_five_nums).to be_a(Array)
+    expect(@key.array_random_five_nums.count).to eq(5)
+  end
+end
