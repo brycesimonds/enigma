a78d5947e7ccdaa921c82c39a1cba52d719eb634
Bryce Simonds <bsimonds14@gmail.com>
2022-06-11 14:22:24 -0600

    Test: it exists

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,14 @@
+require './lib/enigma'
+require 'date'
+
+RSpec.describe Enigma do
+  before :each do
+    @date_without_argument = Time.now.strftime("%m/%d/%y").delete('/') #such as "061122"
+
+    @enigma_1 = Enigma.new
+  end
+
+  it 'exists' do
+    expect(@enigma).to be_a(Enigma)
+  end
+end 
