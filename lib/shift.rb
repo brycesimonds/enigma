ae18a716e18b86c24b9ad5e0246f6fa0be7c9d66
Bryce Simonds <bsimonds14@gmail.com>
2022-06-11 17:26:01 -0600

    Feat: initializes with the proper shifts a through d

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,9 +1,12 @@
 class Shift
-  attr_reader :shift_a, :shift_b, :shift_c, :shift_d
-  def initialize
-    @shift_a =
-    @shift_b =
-    @shift_c =
-    @shift_d =
+  attr_reader :the_shifts
+
+  def initialize(key, offset)
+    @the_shifts = {
+      shift_a: key.the_keys[:key_a].to_i + offset.the_offsets[:offset_a].to_i,
+      shift_b: key.the_keys[:key_b].to_i + offset.the_offsets[:offset_b].to_i,
+      shift_c: key.the_keys[:key_c].to_i + offset.the_offsets[:offset_c].to_i,
+      shift_d: key.the_keys[:key_d].to_i + offset.the_offsets[:offset_d].to_i
+    }
   end
 end
