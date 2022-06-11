7c5b48b5feabd4b31a1560c286888843d5ef3509
Bryce Simonds <bsimonds14@gmail.com>
2022-06-11 14:06:42 -0600

    Feat: offset have offsets a through d

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,9 +1,15 @@
 require "date"
 class Offset
-  attr_reader :date, :the_squared_date
+  attr_reader :date, :the_squared_date, :the_offsets
   def initialize(date = Time.now.strftime("%m/%d/%y").delete('/'))
     @date = date #string
     @the_squared_date = square_date #integer
+    @the_offsets = {
+      offset_a: last_four_nums_of_square_date[0],
+      offset_b: last_four_nums_of_square_date[1],
+      offset_c: last_four_nums_of_square_date[2],
+      offset_d: last_four_nums_of_square_date[3]
+    }
   end
 
   def square_date
