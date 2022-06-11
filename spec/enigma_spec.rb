4af279bff6e1ff97d896bff44010ca4e368eb585
Bryce Simonds <bsimonds14@gmail.com>
2022-06-11 16:34:22 -0600

    Refactor: rearranged order of tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -12,15 +12,15 @@ RSpec.describe Enigma do
     expect(@enigma_1).to be_a(Enigma)
   end
 
+  it 'can return todays date in ddmmyy' do
+    expect(@enigma_1.todays_date_ddmmyy).to eq(Time.now.strftime("%m/%d/%y").delete('/'))
+  end
+
   xit 'can encrypt a message given 3 arguments' do
     expect(@enigma_1.encrypt("hello world", "02715", "040895")).to eq({
       encryption: "keder ohulw",
       key: "02715",
       date: "040895"
       })
-  end
-
-  it 'can return todays date in ddmmyy' do
-    expect(@enigma_1.todays_date_ddmmyy).to eq(Time.now.strftime("%m/%d/%y").delete('/'))
   end
 end
