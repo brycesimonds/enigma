14d95aeb20a0c5425c4ed63318398a7f46a5f1d4
Bryce Simonds <bsimonds14@gmail.com>
2022-06-11 13:22:49 -0600

    Feat: square date method returns a squared integer

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,7 +1,12 @@
 require "date"
 class Offset
   attr_reader :date
-  def initialize(date = Time.now.strftime("%m/%d/%Y").delete('/'))
+  def initialize(date = Time.now.strftime("%m/%d/%y").delete('/'))
     @date = date
+  end
+
+  def square_date
+    return date[1..5].to_i ** 2 if date[0] == "0"
+    return date.to_i ** 2 if date[0] != "0"
   end
 end
