451b0c983edb78785c4dce6ebb584b9a2c22380e
Bryce Simonds <bsimonds14@gmail.com>
2022-06-11 16:42:52 -0600

    Feat: started the encrypt method. Need to fix to change the provided string

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,4 +7,11 @@ class Enigma
   def todays_date_ddmmyy
     Time.now.strftime("%m/%d/%y").delete('/')
   end
+
+  def encrypt(string, key = Kew.new.number, date = todays_date_ddmmyy)
+    @encrypted[:encryption] = string
+    @encrypted[:key] = key
+    @encrypted[:date] = date
+    return @encrypted
+  end
 end
