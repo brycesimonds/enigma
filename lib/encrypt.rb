81718284c772bb7581bb68d3ff8cc68f7908ec44
Bryce Simonds <bsimonds14@gmail.com>
2022-06-12 16:42:16 -0600

    Deleted require statements for now

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,13 +1,7 @@
-require './lib/enigma'
-require './lib/offset'
-require './lib/key'
-require './lib/shift'
-require 'date'
-
 handle = File.open(ARGV[0], "r")
 
 incoming_text = handle.read
 
 handle.close
 
-puts incoming_text
+p incoming_text
