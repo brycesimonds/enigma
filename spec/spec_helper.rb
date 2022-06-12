cdac2bd42742fe0085f298a334ee65ee0854ae82
Bryce Simonds <bsimonds14@gmail.com>
2022-06-12 13:21:22 -0600

    Added engima spec to spec helper rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ SimpleCov.start
 
 require_relative './key_spec'
 require_relative './offset_spec'
+require_relative './enigma_spec'
 #remember to require_relative files as you go
 #use rspec to run
 #open coverage/index.html opens website
