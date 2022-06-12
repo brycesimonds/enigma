39b806e3e9142fb2bc9a17479191c4d791c61f1c
Bryce Simonds <bsimonds14@gmail.com>
2022-06-12 17:19:43 -0600

    Feat: outputs to screen the fole wrote to, the key and date used

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -7,15 +7,22 @@ require 'date'
 enigma = Enigma.new
 
 handle = File.open(ARGV[0], "r")
-
 incoming_text = handle.read
-
 handle.close
-
 encrypted_text = enigma.encrypt(incoming_text.delete("\n"))
 
 writer = File.open(ARGV[1], "w")
-
 writer.write(encrypted_text)
-
 writer.close
+
+reader_key = File.open(ARGV[1], "r")
+key = reader_key.read[-24..-20]
+reader_key.close
+
+reader_date = File.open(ARGV[1], "r")
+date = reader_date.read[-8..-3]
+reader_date.close
+
+
+
+puts "Created '#{ARGV[1]}' with the key #{key} and date #{date}"
