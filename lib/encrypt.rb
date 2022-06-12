a158523f61b89caf887fa797a69eef4ac2682bc5
Bryce Simonds <bsimonds14@gmail.com>
2022-06-12 16:54:46 -0600

    Added back in requires and writer methods

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,7 +1,21 @@
+require './lib/enigma'
+require './lib/offset'
+require './lib/key'
+require './lib/shift'
+require 'date'
+
+enigma = Enigma.new
+
 handle = File.open(ARGV[0], "r")
 
 incoming_text = handle.read
 
 handle.close
 
-p incoming_text
+encrypted_text = enigma.encrypt(incoming_text.delete("\n"))
+
+writer = File.open(ARGV[1], "w")
+
+writer.write(encrypted_text)
+
+writer.close
