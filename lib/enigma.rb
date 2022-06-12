18f42c27ce85eec7d6d47966e9fb592a5ff86051
Bryce Simonds <bsimonds14@gmail.com>
2022-06-12 13:52:07 -0600

    Deleted Feats: deleted helper methods as they were breaking my encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -19,20 +19,6 @@ class Enigma
     ("a".."z").to_a << " "
   end
 
-  def string_to_key_instance(key)
-    original_key_string = key
-    key = Key.new(original_key_string)
-  end
-
-  def array_of_shifts(key, date)
-    if key.class == String
-      original_key_string = key
-      key = Key.new(original_key_string)
-    end
-    shift = Shift.new(key, Offset.new(date))
-        array_of_shifts = [shift.the_shifts[:shift_a], shift.the_shifts[:shift_b], shift.the_shifts[:shift_c], shift.the_shifts[:shift_d]]
-  end
-
   def encrypt_string(string, key = Key.new, date = todays_date_ddmmyy)
     if key.class == String
       original_key_string = key
