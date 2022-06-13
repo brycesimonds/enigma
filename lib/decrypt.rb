3a226e6eb506f6934af14d2c7d7926940dd0c23b
Bryce Simonds <bsimonds14@gmail.com>
2022-06-13 14:28:19 -0600

    Completed the runner with its four command line arguments

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -14,3 +14,14 @@ array_of_encryption_key_date = incoming_string.split(",")
 encryption_key_value_string = array_of_encryption_key_date[0]
 seperate_encryption_from_message = encryption_key_value_string.split("=>")
 the_encrypted_message = seperate_encryption_from_message[1]
+
+key = ARGV[2]
+date = ARGV[3]
+
+decrypted_text = enigma.decrypt(the_encrypted_message, key, date)
+
+writer = File.open(ARGV[1], "w")
+writer.write(decrypted_text)
+writer.close
+
+puts "Created '#{ARGV[1]}' with the key #{key} and date #{date}"
