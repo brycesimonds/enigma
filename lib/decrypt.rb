9e536d20f288ab2f84040a92f41caa7fa1b13fcc
Bryce Simonds <bsimonds14@gmail.com>
2022-06-13 14:27:35 -0600

    Retrieve the encrypted string

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -9,3 +9,8 @@ enigma = Enigma.new
 handle = File.open(ARGV[0], "r")
 incoming_string = handle.read.delete('\\"')
 handle.close
+
+array_of_encryption_key_date = incoming_string.split(",")
+encryption_key_value_string = array_of_encryption_key_date[0]
+seperate_encryption_from_message = encryption_key_value_string.split("=>")
+the_encrypted_message = seperate_encryption_from_message[1]
