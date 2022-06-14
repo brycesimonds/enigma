40a33838a84f8b495180f12d5b7201d706b4d2bf
Bryce Simonds <bsimonds14@gmail.com>
2022-06-14 15:19:40 -0600

    Refactor: deleted comments

--- lib/key.rb
+++ lib/key.rb
@@ -4,18 +4,18 @@ class Key
   def initialize(number = random_num_string)
     @number = number
     @the_keys = {
-      key_a: number[0..1], #these will produce sections of the strings,
-      key_b: number[1..2], #like "02", or "45"
+      key_a: number[0..1],
+      key_b: number[1..2],
       key_c: number[2..3],
       key_d: number[3..4]
     }
   end
 
-  def array_random_five_nums #returns something like [1, 0, 6, 5, 2]
+  def array_random_five_nums
     (0..9).to_a.sample(5)
   end
 
-  def random_num_string #returns random 5 digit STRING, "46543"
+  def random_num_string 
     array_random_five_nums.join(" ").delete(' ')
   end
 end
